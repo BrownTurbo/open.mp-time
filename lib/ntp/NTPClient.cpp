b7e7a10b7c6f388d03860596e9ead1750152a705
John Magdy Lotfy Kamel <19735243+Zorono@users.noreply.github.com>
2026-04-25 11:27:43 +0300

    WIP ?

--- lib/ntp/NTPClient.cpp
+++ lib/ntp/NTPClient.cpp
@@ -255,6 +255,15 @@ void NTPClient::setPoolServerName(const char* poolServerName) {
     this->_poolServerName = poolServerName;
 }
 
+void NTPClient::setPoolServerIP(IPAddress poolServerIP) {
+    this->_poolServerIP = poolServerIP;
+}
+
+void NTPClient::setPoolServerPort(unsigned int port)
+{
+  this->_port = port;
+}
+
 void NTPClient::sendNTPPacket() {
   // set all bytes in the buffer to 0
   memset(this->_packetBuffer, 0, NTP_PACKET_SIZE);
